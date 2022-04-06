bbbe37d3f5c7f2b7121248bfc459c521c27df3a3
Arka021 <arkaghosh021@gmail.com>
2022-04-06 17:01:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-tute';
+  title = 'This is my 1st tutorial';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice',title:'developer' },
+  { id: 12, name: 'Narco', title:'desiener'},
+  { id: 13, name: 'Bombasto', title:'content writer' },
+  { id: 14, name: 'Celeritas', title:'writter' },
+  { id: 15, name: 'Magneta', title:'serviece' },
+  { id: 16, name: 'RubberMan', title:'network enginner' },
+  { id: 17, name: 'Dynama', title:'specker' },
+  { id: 18, name: 'Dr IQ', title:'HR' },
+  { id: 19, name: 'Magma', title:'singer' },
+  { id: 20, name: 'Tornado', title:'developer' }
+];
